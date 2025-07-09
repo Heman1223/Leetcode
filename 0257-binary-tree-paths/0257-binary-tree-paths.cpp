212cd7e73e318f37817a7ee9c3ca4b04e4522f7b
Heman1223 <152138748+Heman1223@users.noreply.github.com>
2025-07-09 09:39:42 +0530

    Time: 0 ms (100%), Space: 17.9 MB (26.96%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.cpp
+++ 0257-binary-tree-paths/0257-binary-tree-paths.cpp
@@ -14,23 +14,20 @@ private:
     void findpath(TreeNode* node,vector<string>& ans,string temp){
         if(!node) return;
         temp = temp + to_string(node->val);
-        //if(node -> left){
-            findpath(node->left,ans,temp + "->");
-       // }
-        //if(node -> right){
-            findpath(node->right,ans,temp + "->");
-        //}
+        
+        findpath(node->left,ans,temp + "->"); 
+        findpath(node->right,ans,temp + "->");
+        
         if(!node->left && !node->right){
             ans.push_back(temp);
         }
     }
 public:
     vector<string> binaryTreePaths(TreeNode* root) {
-        vector<string> v;
-        if(root){
-            findpath(root,v,"");
-        }
-        return v;
+        vector<string> ans;
+
+        findpath(root,ans,"");
+        return ans;
 
     }
 };
