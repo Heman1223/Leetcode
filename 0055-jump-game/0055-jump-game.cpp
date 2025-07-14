e7d066d229c66fd6599155a13d57a9e6bc3fdc9b
Heman Sharma <152138748+Heman1223@users.noreply.github.com>
2025-07-14 12:07:36 +0530

    Time: 1 ms (33.88%), Space: 52.3 MB (33.09%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        int reached = n - 1;
+
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i] + i >= reached) {
+                reached = i;
+            }
+        }
+        return reached == 0;
+    }
+};
