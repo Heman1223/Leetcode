f74b66a23d42f944c492acc6373daee9c9a41382
Heman Sharma <152138748+Heman1223@users.noreply.github.com>
2025-07-14 10:01:19 +0530

    Time: 0 ms (100%), Space: 8.4 MB (52.01%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,31 @@
+class Solution {
+private:
+    //--RECURSION + MEMOISATION--
+    // int solve(int n,vector<int> &dp){
+    //     if(n == 0){
+    //         return 1;
+    //     }
+    //     if(n < 0){
+    //         return 0;
+    //     }
+    //     if(dp[n] != -1){
+    //         return dp[n];
+    //     }
+    //     dp[n] = solve(n - 1,dp) + solve(n - 2,dp);
+    //     return dp[n];
+    // }
+
+public:
+    //--TABULATION--
+    int climbStairs(int n) {
+        if(n == 0) return 1;
+        if(n == 1 ) return 1;
+        vector<int> dp(n + 1,0);
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i = 2;i<= n;i++){
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+};
