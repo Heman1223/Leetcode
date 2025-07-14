89a1a789a87211bb5c33e1d19239a90287383343
Heman Sharma <152138748+Heman1223@users.noreply.github.com>
2025-07-14 09:42:09 +0530

    Time: 3 ms (66.64%), Space: 8.3 MB (11.24%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,12 +1,20 @@
 class Solution {
 public:
-    int fib(int n) {
+    int func(int n,vector<int> &dp){
         if(n == 0){
             return 0;
         }
         if(n == 1){
             return 1;
         }
-        return fib(n - 1) + fib(n - 2);
+        if(dp[n] != -1){
+            return dp[n];
+        }
+        dp[n] = func(n - 1,dp) + func(n - 2,dp);
+        return dp[n];
+    }
+    int fib(int n) {
+        vector<int> dp(n + 1,-1);
+        return func(n,dp);
     }
 };
