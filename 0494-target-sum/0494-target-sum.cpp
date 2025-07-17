91032ec3127ea66132fd7c501da9544a093abee0
Heman Sharma <152138748+Heman1223@users.noreply.github.com>
2025-07-17 09:20:12 +0530

    Time: 3 ms (87.84%), Space: 15.6 MB (44.76%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -10,17 +10,17 @@ public:
             return 0;
             }
 
-            int W = (target+sum)/2;
-            vector<vector<int>> dp(n+1,vector<int>(W+1,0));
+            target = (target+sum)/2;
+            vector<vector<int>> dp(n+1,vector<int>(target+1,0));
             dp[0][0] = 1;
 
             for(int i=1;i<=n;i++){
-            for(int j=0;j<=W;j++){
+            for(int j=0;j<=target;j++){
             if(nums[i-1] > j) dp[i][j] = dp[i-1][j];
             else dp[i][j] = dp[i-1][j] + dp[i-1][j-nums[i-1]];
             }
             }
 
-            return dp[n][W];
+            return dp[n][target];
     }
 };
