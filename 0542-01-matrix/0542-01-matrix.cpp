0b7a712eadf4123dc05d0de3e5d9c8279347f9a0
Heman Sharma <152138748+Heman1223@users.noreply.github.com>
2025-07-19 14:52:12 +0530

    Time: 94 ms (8.28%), Space: 60 MB (6.26%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int row = mat.size();
+        int col = mat[0].size();
+        queue<vector<int>> q;
+        int zeroes = 0;
+        for(int i = 0 ;i<row;i++){
+            for(int j = 0;j<col;j++){
+                if(mat[i][j] == 0){
+                    q.push({i,j});
+                    zeroes++;
+                }else{
+                    mat[i][j] = INT_MAX;
+                }
+            }
+        }
+        vector<int> dx = {0,0,1,-1};
+        vector<int> dy = {1,-1,0,0};
+
+        while(!q.empty()){
+            int size = q.size();
+            for(int i = 0;i<size;i++){
+                vector<int> node = q.front();
+                q.pop();
+                for(int j=0;j < 4;j++){
+                    int x = node[0] + dx[j];
+                    int y = node[1] + dy[j];
+                    if (x >= 0 && y >= 0 && x < row && y < col) {
+                        if (mat[x][y] > mat[node[0]][node[1]] + 1) {
+                            mat[x][y] = mat[node[0]][node[1]] + 1;
+                            q.push({x, y});
+                        }
+                    }
+                }
+            }
+        }
+        return mat;
+    }
+};
