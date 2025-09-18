8cc0895863feb05aebf9c1437bbb5ace4e5bd2ba
Heman Sharma <152138748+Heman1223@users.noreply.github.com>
2025-09-18 13:07:00 +0530

    Time: 110 ms (6.71%), Space: 60.1 MB (5.94%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -4,33 +4,29 @@ public:
         int row = mat.size();
         int col = mat[0].size();
         queue<vector<int>> q;
-        int zeroes = 0;
-        for(int i = 0 ;i<row;i++){
-            for(int j = 0;j<col;j++){
+        for(int i = 0 ; i < row ; i++){
+            for(int j = 0 ; j < col ; j++){
                 if(mat[i][j] == 0){
                     q.push({i,j});
-                    zeroes++;
                 }else{
                     mat[i][j] = -1;
                 }
             }
         }
-        vector<int> dx = {0,0,1,-1};
-        vector<int> dy = {1,-1,0,0};
-
+        vector<int> dx = {1,-1,0,0};
+        vector<int> dy = {0,0,1,-1};
         while(!q.empty()){
             int size = q.size();
-            for(int i = 0;i<size;i++){
+            for(int i = 0;i < size ;i++){
                 vector<int> node = q.front();
                 q.pop();
-                for(int j=0;j < 4;j++){
+                for(int j = 0 ; j < 4;j++){
                     int x = node[0] + dx[j];
                     int y = node[1] + dy[j];
-                    if (x >= 0 && y >= 0 && x < row && y < col && mat[x][y] == -1) {
-                        mat[x][y] = mat[node[0]][node[1]] + 1;
-                        q.push({x, y});
+                    if(x >= 0 && x < row && y >= 0 && y < col && mat[x][y] == -1){
+                        q.push({x,y});
+                        mat[x][y] = mat[node[0]][node[1]] + 1; 
                     }
-                    
                 }
             }
         }
