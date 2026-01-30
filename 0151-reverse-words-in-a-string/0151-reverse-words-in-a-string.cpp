a3cc5495743b60c59b8e250bb8a4513045e252ea
Heman Sharma <152138748+Heman1223@users.noreply.github.com>
2026-01-30 12:49:55 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        vector<string> arr;
+        string str = "";
+        for(char i : s){
+            if(isalpha(i)){
+                str += i;
+            }else if(i == ' '){
+                if(str.length() == 0){
+                    continue;
+                }
+                arr.push_back(str);
+                str = "";
+            }
+        }
+        if(str.length() != 0){
+            arr.push_back(str);
+        }
+        string ans = "";
+        for(int i = arr.size() - 1;i >0;i--){
+            ans = ans + arr[i] + " ";
+        }
+        ans = ans + arr[0];
+        return ans;
+    }
+};
