5ea9a91207efd7ddbade7888e28f8af435b790b8
Heman Sharma <152138748+Heman1223@users.noreply.github.com>
2026-02-04 10:12:10 +0530

    Time: 0 ms (100%), Space: 7.6 MB (96.09%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        if(n == 1 || n == 7) return true;
+        if(n < 10) return false;
+        int temp = n;
+        int sum = 0;
+        while(temp > 0){
+            int digit = temp % 10;
+            sum += (digit * digit);
+            temp /= 10;
+        }
+        return isHappy(sum);
+    }
+};
