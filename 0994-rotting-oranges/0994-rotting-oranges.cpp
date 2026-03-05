71fbd58d4b1410767f96c7c909b3609245c4b5a3
Heman Sharma <152138748+Heman1223@users.noreply.github.com>
2026-03-05 23:52:19 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -6,9 +6,9 @@ public:
         int fresh = 0;
         int min = 0;
 
-        queue<vector<int>> q;
+        queue<pair<int,int>> q;
         for(int i = 0;i < row;i++){
-            for(int j = 0; j < col; j++){
+            for(int j = 0;j < col;j++){
                 if(grid[i][j] == 2){
                     q.push({i,j});
                 }else if(grid[i][j] == 1){
@@ -16,27 +16,25 @@ public:
                 }
             }
         }
-        if(fresh == 0) return 0;
-        
         vector<int> dx = {0,0,1,-1};
         vector<int> dy = {1,-1,0,0};
-
         while(!q.empty()){
             int size = q.size();
-            for(int i = 0;i < size;i++){
-                vector<int> node = q.front();
+            for(int j = 0 ; j < size;j++){
+                pair<int,int> node = q.front();
                 q.pop();
-                for(int j = 0;j < 4;j++){
-                    int x = node[0] + dx[j];
-                    int y = node[1] + dy[j];
-                    if(x >= 0 && x < row && y >= 0 && y < col && grid[x][y] == 1 ){
+                for(int i = 0;i < 4;i++){
+                    int x = dx[i] + node.first;
+                    int y = dy[i] + node.second;
+                    
+                    if(x >= 0 && x < row && y >= 0 && y < col && grid[x][y] == 1){
                         grid[x][y] = 2;
-                        q.push({x,y});
                         fresh--;
+                        q.push({x,y});
                     }
                 }
             }
-            if(!q.empty()) min ++;
+            if(!q.empty()) min++;
         }
         if(fresh == 0) return min;
         return -1;
