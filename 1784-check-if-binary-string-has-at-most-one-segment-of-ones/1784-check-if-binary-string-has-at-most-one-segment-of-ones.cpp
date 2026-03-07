78e95ddcdd7ba89d11f7d27c9e79f1bd1c29bcd5
Heman Sharma <152138748+Heman1223@users.noreply.github.com>
2026-03-07 08:39:10 +0530

    Time: 0 ms (100%), Space: 8.1 MB (76.49%) - LeetHub

---
+++ 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool checkOnesSegment(string s) {
+        bool flag = false;
+
+
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='1' && flag == true){
+                return false;
+               
+            }
+           if(s[i]=='0'){
+            flag = true;
+           }
+        }
+        return true;
+    }
+};
