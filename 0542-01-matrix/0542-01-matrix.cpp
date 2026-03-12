5e93d3737f2cf407ea4f27ea83d5e0502109af08
Heman Sharma <152138748+Heman1223@users.noreply.github.com>
2026-03-13 00:10:50 +0530

    Time: 38 ms (11.87%), Space: 38.8 MB (30.2%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,35 +1,43 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int row = mat.size();
-        int col = mat[0].size();
-        queue<vector<int>> q;
-        for(int i = 0 ; i < row ; i++){
-            for(int j = 0 ; j < col ; j++){
+        int n = mat.size();
+        int m = mat[0].size();
+
+        vector<vector<int>> vis(n,vector<int>(m,0));
+        vector<vector<int>> dist(n,vector<int>(m,0));
+        
+        queue<pair<pair<int,int>,int>> q;
+
+        for(int i = 0;i < n;i++){
+            for(int j = 0; j < m;j++){
                 if(mat[i][j] == 0){
-                    q.push({i,j});
-                }else{
-                    mat[i][j] = -1;
+                    q.push({{i,j},0});
+                    vis[i][j] = 1;
                 }
             }
         }
-        vector<int> dx = {1,-1,0,0};
-        vector<int> dy = {0,0,1,-1};
+
+        int dx[] = {1,-1,0,0};
+        int dy[] = {0,0,1,-1};
         while(!q.empty()){
-            int size = q.size();
-            for(int i = 0;i < size ;i++){
-                vector<int> node = q.front();
-                q.pop();
-                for(int j = 0 ; j < 4;j++){
-                    int x = node[0] + dx[j];
-                    int y = node[1] + dy[j];
-                    if(x >= 0 && x < row && y >= 0 && y < col && mat[x][y] == -1){
-                        q.push({x,y});
-                        mat[x][y] = mat[node[0]][node[1]] + 1; 
-                    }
+            int row = q.front().first.first;
+            int col = q.front().first.second;
+            int steps = q.front().second;
+            q.pop();
+
+            dist[row][col] = steps;
+            for(int i = 0;i < 4;i++){
+                int x = row + dx[i];
+                int y = col + dy[i];
+
+                if(x >= 0 && x < n && y >= 0 && y < m && vis[x][y] == 0){
+                    q.push({{x,y}, steps + 1});
+                    vis[x][y] = 1;
                 }
             }
         }
-        return mat;
+
+        return dist;
     }
 };
