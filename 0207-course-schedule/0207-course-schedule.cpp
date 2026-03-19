ae19de6190ccb6cd1867fd12c7f652c566c4fd77
Heman Sharma <152138748+Heman1223@users.noreply.github.com>
2026-03-19 15:00:27 +0530

    Time: 3 ms (76.68%), Space: 19.3 MB (55.35%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,38 +1,40 @@
 class Solution {
 public:
+    bool dfs(int node,vector<vector<int>> &adj,vector<int> &vis,vector<int> &pathvis){
+        vis[node] = 1;
+        pathvis[node] = 1;
+        for(auto it : adj[node]){
+            if(!vis[it]){
+                if(dfs(it,adj,vis,pathvis)){
+                    return true;
+                }
+            }else if(pathvis[it]){
+                return true;
+            }
+        }
+        pathvis[node] = 0;
+   
+        return false;
+    }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>> adj(numCourses);
-        for(auto &edge : prerequisites){
-            int u = edge[0];
-            int v = edge[1];
-            adj[u].push_back(v);
-        }
+        for(int i = 0;i < prerequisites.size();i++){
+            int u = prerequisites[i][0];
+            int v = prerequisites[i][1];
 
-        vector<int> inDegree(numCourses,0);
-        for(int i = 0;i < numCourses;i++){
-            for(int it : adj[i]){
-                inDegree[it]++;
-            }
+            adj[v].push_back(u);
         }
+        vector<int> vis(numCourses,0);
+        vector<int> pathvis(numCourses,0);
 
-        queue<int> q;
-        for(int i = 0;i < numCourses;i++){
-            if(inDegree[i] == 0){
-                q.push(i);
-            }
-        }
-        vector<int> topo;
-        while(!q.empty()){
-            int node = q.front();
-            q.pop();
-            topo.push_back(node);
-            for(int it : adj[node]){
-                inDegree[it]--;
-                if(inDegree[it] == 0){
-                    q.push(it);
+
+        for(int i = 0 ; i < numCourses;i++){
+            if(!vis[i]){
+                if(dfs(i,adj,vis,pathvis)){
+                    return false;
                 }
             }
         }
-        return topo.size() == numCourses;
+        return true;
     }
 };
