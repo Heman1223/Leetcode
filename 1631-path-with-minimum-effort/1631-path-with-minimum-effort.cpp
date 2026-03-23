c09a386b79e499df8979a7f8d7994853405c0c05
Heman Sharma <152138748+Heman1223@users.noreply.github.com>
2026-03-23 13:30:38 +0530

    Time: 47 ms (75.95%), Space: 24.6 MB (56.38%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -1,36 +1,37 @@
 class Solution {
 public:
     int minimumEffortPath(vector<vector<int>>& heights) {
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>> pq;
+        
         int rows = heights.size();
         int cols = heights[0].size();
 
-        vector<vector<int>> dist(rows,vector<int>(cols,INT_MAX));
-        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>> pq;
-        pq.push({0,{0,0}});
-        dist[0][0] = 0;
+        vector<vector<int>> dist(rows,vector<int>(cols,1e9));
 
-        int dx[] = {0,0,1,-1};
-        int dy[] = {1,-1,0,0};
+         pq.push({0,{0,0}});
+         dist[0][0] = 0;
+
+        int dx[] = {1,-1,0,0};
+        int dy[] = {0,0,1,-1};
 
         while(!pq.empty()){
             auto it = pq.top();
+            int effort = it.first;
+            int currx = it.second.first;
+            int curry = it.second.second;
             pq.pop();
 
-            int effort = it.first;
-            int x = it.second.first;
-            int y = it.second.second;
-            
-            if(x == rows - 1 && y == cols - 1) return effort;
-            for(int i = 0;i < 4 ;i++){
-                int newx = x + dx[i];
-                int newy = y + dy[i];
-
-                if(newx >= 0 && newx < rows && newy >= 0 && newy < cols){
-                    int newEffort = max(effort,abs(heights[x][y] - heights[newx][newy]));
-
-                    if(newEffort < dist[newx][newy]){
-                        dist[newx][newy] = newEffort;
-                        pq.push({newEffort,{newx,newy}});
+            if(currx == rows - 1 && curry == cols - 1){
+                return effort;
+            }
+            for(int i = 0;i < 4;i++){
+                int nextx = currx + dx[i];
+                int nexty = curry + dy[i];
+                if(nextx >= 0 && nextx < rows && nexty >= 0 && nexty < cols){
+                    int maxeffort = max(effort,abs(heights[currx][curry] - heights[nextx][nexty]));
+                    if(maxeffort < dist[nextx][nexty]){
+                        dist[nextx][nexty] = maxeffort;
+                        pq.push({maxeffort,{nextx,nexty}});
                     }
                 }
             }
