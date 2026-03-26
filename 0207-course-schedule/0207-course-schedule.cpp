dcfc4728ccf5aa7063ebfd25adcd94cad46def1a
Heman Sharma <152138748+Heman1223@users.noreply.github.com>
2026-03-27 00:29:48 +0530

    Time: 3 ms (76.54%), Space: 20.2 MB (19.02%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
-    bool dfs(int node,vector<vector<int>> &adj,vector<int> &vis,vector<int> &pathvis){
+    bool dfs(int node,vector<int> &vis,vector<int> &pathvis,vector<vector<int>> &adj){
         vis[node] = 1;
         pathvis[node] = 1;
         for(auto it : adj[node]){
             if(!vis[it]){
-                if(dfs(it,adj,vis,pathvis)){
+                if(dfs(it,vis,pathvis,adj)){
                     return true;
                 }
             }else if(pathvis[it]){
@@ -13,24 +13,20 @@ public:
             }
         }
         pathvis[node] = 0;
-   
         return false;
     }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>> adj(numCourses);
-        for(int i = 0;i < prerequisites.size();i++){
-            int u = prerequisites[i][0];
-            int v = prerequisites[i][1];
-
+        for(auto it : prerequisites){
+            int u = it[0];
+            int v = it[1];
             adj[v].push_back(u);
         }
         vector<int> vis(numCourses,0);
         vector<int> pathvis(numCourses,0);
-
-
-        for(int i = 0 ; i < numCourses;i++){
+        for(int i = 0;i < numCourses;i++){
             if(!vis[i]){
-                if(dfs(i,adj,vis,pathvis)){
+                if(dfs(i,vis,pathvis,adj)){
                     return false;
                 }
             }
