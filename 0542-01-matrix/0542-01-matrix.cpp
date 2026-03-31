414cd1626d4eb9cb2af0fa58bb17691a390e91a8
Heman Sharma <152138748+Heman1223@users.noreply.github.com>
2026-03-31 23:46:47 +0530

    Time: 16 ms (66.03%), Space: 39 MB (20.26%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -3,41 +3,39 @@ public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int n = mat.size();
         int m = mat[0].size();
-
         vector<vector<int>> vis(n,vector<int>(m,0));
         vector<vector<int>> dist(n,vector<int>(m,0));
-        
-        queue<pair<pair<int,int>,int>> q;
 
+        queue<pair<pair<int,int>,int>> q;
         for(int i = 0;i < n;i++){
-            for(int j = 0; j < m;j++){
+            for(int j = 0;j < m;j++){
                 if(mat[i][j] == 0){
                     q.push({{i,j},0});
                     vis[i][j] = 1;
                 }
             }
         }
+        int dx[] = {0,0,1,-1};
+        int dy[] = {1,-1,0,0};
 
-        int dx[] = {1,-1,0,0};
-        int dy[] = {0,0,1,-1};
         while(!q.empty()){
-            int row = q.front().first.first;
-            int col = q.front().first.second;
+            int currx = q.front().first.first;
+            int curry = q.front().first.second;
             int steps = q.front().second;
             q.pop();
 
-            dist[row][col] = steps;
-            for(int i = 0;i < 4;i++){
-                int x = row + dx[i];
-                int y = col + dy[i];
+            dist[currx][curry] = steps;
 
-                if(x >= 0 && x < n && y >= 0 && y < m && vis[x][y] == 0){
-                    q.push({{x,y}, steps + 1});
-                    vis[x][y] = 1;
+            for(int i = 0;i < 4;i++){
+                int nextx = currx + dx[i];
+                int nexty = curry + dy[i];
+
+                if(nextx >= 0 && nextx < n && nexty >= 0 && nexty < m && vis[nextx][nexty] == 0){
+                    q.push({{nextx,nexty}, steps + 1});
+                    vis[nextx][nexty] = 1;
                 }
             }
         }
-
         return dist;
     }
 };
