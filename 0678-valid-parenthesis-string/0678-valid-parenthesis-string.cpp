fc32fb602b3691acc6101725d45c1d66bc0b2838
Heman Sharma <152138748+Heman1223@users.noreply.github.com>
2026-04-15 00:07:48 +0530

    Time: 0 ms (100%), Space: 8 MB (76.21%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int low = 0;
+        int high = 0;
+        for(char c : s){
+            if(c == '('){
+                high++;
+                low++;
+            }else if(c == ')'){
+                high--;
+                low--;
+            }else{
+                low--;
+                high++;
+            }
+            if(low < 0) low = 0;
+            if(high < 0) return false;
+        }
+        return low == 0;
+    }
+};
