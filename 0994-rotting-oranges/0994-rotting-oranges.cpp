daeb4bf330c19c4d3ab4b465d7f102660b9dc13d
Heman Sharma <152138748+Heman1223@users.noreply.github.com>
2026-04-15 14:21:02 +0530

    Time: 0 ms (100%), Space: 16.8 MB (52.21%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int row = grid.size();
-        int col = grid[0].size();
+        int rows = grid.size();
+        int cols = grid[0].size();
+
+        queue<pair<int,int>> q;
         int fresh = 0;
         int min = 0;
 
-        queue<pair<int,int>> q;
-        for(int i = 0;i < row;i++){
-            for(int j = 0;j < col;j++){
+        for(int i = 0 ; i < rows;i++){
+            for(int j = 0;j < cols;j++){
                 if(grid[i][j] == 2){
                     q.push({i,j});
                 }else if(grid[i][j] == 1){
@@ -16,21 +17,24 @@ public:
                 }
             }
         }
-        vector<int> dx = {0,0,1,-1};
-        vector<int> dy = {1,-1,0,0};
+        int dx[] = {1,-1,0,0};
+        int dy[] = {0,0,1,-1};
+
         while(!q.empty()){
             int size = q.size();
-            for(int j = 0 ; j < size;j++){
-                pair<int,int> node = q.front();
+            for(int i = 0 ; i < size;i++){
+                auto it = q.front();
+                int currx = it.first;
+                int curry = it.second;
                 q.pop();
-                for(int i = 0;i < 4;i++){
-                    int x = dx[i] + node.first;
-                    int y = dy[i] + node.second;
-                    
-                    if(x >= 0 && x < row && y >= 0 && y < col && grid[x][y] == 1){
-                        grid[x][y] = 2;
+                for(int j = 0;j < 4;j++){
+                    int nextx = dx[j] + currx;
+                    int nexty = dy[j] + curry;
+
+                    if(nexty >= 0 && nextx >= 0 && nextx < rows && nexty < cols && grid[nextx][nexty] == 1){
+                        grid[nextx][nexty] = 2;
+                        q.push({nextx,nexty});
                         fresh--;
-                        q.push({x,y});
                     }
                 }
             }
