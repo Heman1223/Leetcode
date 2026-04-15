5793cc9819d23f691f4d4ba8d564f69ac4833969
Heman Sharma <152138748+Heman1223@users.noreply.github.com>
2026-04-15 15:06:07 +0530

    Time: 13 ms (76.15%), Space: 37.6 MB (46.4%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -3,36 +3,39 @@ public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int n = mat.size();
         int m = mat[0].size();
-        vector<vector<int>> vis(n,vector<int>(m,0));
-        vector<vector<int>> dist(n,vector<int>(m,0));
 
-        queue<pair<pair<int,int>,int>> q;
-        for(int i = 0;i < n;i++){
+        vector<vector<int>> dist(n,vector<int> (m,0));
+        vector<vector<int>> vis(n,vector<int> (m,0));
+
+        queue<pair<int,int>> q;
+        for(int i = 0 ;i < n;i++ ){
             for(int j = 0;j < m;j++){
                 if(mat[i][j] == 0){
-                    q.push({{i,j},0});
+                    q.push({i,j});
+                    dist[i][j] = 0;
                     vis[i][j] = 1;
                 }
             }
         }
-        int dx[] = {0,0,1,-1};
-        int dy[] = {1,-1,0,0};
-
+        int dx[] = {1,-1,0,0};
+        int dy[] = {0,0,-1,1};
         while(!q.empty()){
-            int currx = q.front().first.first;
-            int curry = q.front().first.second;
-            int steps = q.front().second;
-            q.pop();
+            int size = q.size();
+            for(int i = 0 ; i < size;i++){
+                auto it = q.front();
+                int x = it.first;
+                int y = it.second;
+                int dst = dist[x][y];
+                q.pop();
 
-            dist[currx][curry] = steps;
-
-            for(int i = 0;i < 4;i++){
-                int nextx = currx + dx[i];
-                int nexty = curry + dy[i];
-
-                if(nextx >= 0 && nextx < n && nexty >= 0 && nexty < m && vis[nextx][nexty] == 0){
-                    q.push({{nextx,nexty}, steps + 1});
-                    vis[nextx][nexty] = 1;
+                for(int j = 0 ; j < 4 ;j++){
+                    int currx = dx[j] + x;
+                    int curry = dy[j] + y;
+                    if(currx >= 0 && currx < n && curry >= 0 && curry < m && vis[currx][curry] == 0){
+                        vis[currx][curry] = 1;
+                        dist[currx][curry] = dst + 1;
+                        q.push({currx,curry});
+                    } 
                 }
             }
         }
