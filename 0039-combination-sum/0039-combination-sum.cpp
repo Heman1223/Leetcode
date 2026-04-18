2a463cbe39d13f2cc9521a7f5c0150cca4f81ad9
Heman Sharma <152138748+Heman1223@users.noreply.github.com>
2026-04-19 00:35:43 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,24 +1,23 @@
 class Solution {
-private:
-    void function(int start,vector<int>& candidates,int target,vector<int> comb,vector<vector<int>> &ans){
+public:
+    void function(vector<int>& candidates,vector<vector<int>> &ans,vector<int> comb,int target,int i){
         if(target == 0){
             ans.push_back(comb);
             return;
         }
-        for(int i = start;i < candidates.size();i++){
-            if(candidates[i] <= target){
-                comb.push_back(candidates[i]);
-                function(i,candidates,target - candidates[i],comb,ans);
-                comb.pop_back();
-            }
-
+        if(target < 0 || i == candidates.size()){
+            return;
         }
+        comb.push_back(candidates[i]);
+        function(candidates,ans,comb,target - candidates[i],i);
+        comb.pop_back();
+        function(candidates,ans,comb,target,i + 1);
+        return;
     }
-public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<int> comb;
         vector<vector<int>> ans;
-        function(0,candidates,target,comb,ans);
+        vector<int> comb;
+        function(candidates,ans,comb,target,0);
         return ans;
     }
 };
