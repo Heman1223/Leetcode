45d00c730ae410c09cd13bfc20a85733af5cc0df
Heman Sharma <152138748+Heman1223@users.noreply.github.com>
2026-04-21 23:24:46 +0530

    Time: 30 ms (7.18%), Space: 21.5 MB (10.3%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,28 +1,23 @@
 class Solution {
+
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-        int m = matrix[0].size();
-        queue<pair<int,int>> q;
-        for(int i = 0;i < n;i++){
-            for(int j = 0;j < m;j++){
+        int row = matrix.size();
+        int col = matrix[0].size();
+        vector<vector<int>> ans(row,vector<int> (col));
+        ans = matrix;
+        for(int i = 0;i < row;i++){
+            for(int j = 0;j < col;j++){
                 if(matrix[i][j] == 0){
-                    q.push({i,j});
-                    
+                    for(int k = 0;k < col;k++){
+                        ans[i][k] = 0;
+                    }
+                    for(int k = 0;k < row;k++){
+                        ans[k][j] = 0;
+                    }
                 }
             }
         }
-        while(!q.empty()){
-            int r = q.front().first;
-            int c = q.front().second;
-            q.pop();
-            for(int i = 0; i < n;i++){
-                matrix[i][c] = 0;
-            }
-            for(int i = 0;i < m;i++){
-                matrix[r][i] = 0;
-            }
-        }
-
+        matrix = ans;
     }
 };
