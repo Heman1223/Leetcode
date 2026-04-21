2a6e8b18ac9ba893daa6891db3c53eb67673bfb8
Heman Sharma <152138748+Heman1223@users.noreply.github.com>
2026-04-21 23:23:13 +0530

    Time: 27 ms (10.59%), Space: 21.4 MB (11.46%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,28 +1,28 @@
 class Solution {
-
-private:
-    void solve(vector<vector<int>> matrix,vector<vector<int>>& ans,int col,int row,int cols,int rows){
-        if(col >= cols || row >= rows){
-            return;
-        }
-        if(matrix[row][col] == 0){
-            for(int i = 0;i<cols;i++){
-                    ans[row][i] = 0;
-            }
-            for(int j=0;j<rows;j++){
-                    ans[j][col] = 0;
-                }
-        }
-        solve(matrix,ans,col + 1,row,cols,rows);
-        solve(matrix,ans,col,row + 1,cols,rows);
-        
-    }
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int rows=matrix.size();
-        int cols=matrix[0].size();
-        vector<vector<int>> ans = matrix;
-        solve(matrix,ans,0,0,cols,rows);
-        matrix = ans;
+        int n = matrix.size();
+        int m = matrix[0].size();
+        queue<pair<int,int>> q;
+        for(int i = 0;i < n;i++){
+            for(int j = 0;j < m;j++){
+                if(matrix[i][j] == 0){
+                    q.push({i,j});
+                    
+                }
+            }
+        }
+        while(!q.empty()){
+            int r = q.front().first;
+            int c = q.front().second;
+            q.pop();
+            for(int i = 0; i < n;i++){
+                matrix[i][c] = 0;
+            }
+            for(int i = 0;i < m;i++){
+                matrix[r][i] = 0;
+            }
+        }
+
     }
 };
