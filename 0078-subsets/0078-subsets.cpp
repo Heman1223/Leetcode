121c6d9fc2a740838bbb3c167ba0ce1edf24a0a7
Heman Sharma <152138748+Heman1223@users.noreply.github.com>
2026-04-27 22:47:37 +0530

    Time: 0 ms (100%), Space: 10 MB (58.06%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
-    void function(vector<int>& nums,vector<vector<int>> &ans,vector<int> &temp,int start){
-        ans.push_back(temp);
-        for(int i = start;i < nums.size();i++){
-            temp.push_back(nums[i]);
-            function(nums,ans,temp,i + 1);
-            temp.pop_back();
+    void func(int i, vector<int>& temp,vector<vector<int>>& ans,vector<int>& nums){
+        if(i == nums.size()){
+            ans.push_back(temp);
+            return;
         }
+        
+        temp.push_back(nums[i]);
+        func(i + 1,temp,ans,nums);
+        temp.pop_back();
+        func(i + 1,temp,ans,nums);
+       // temp.pop_back();
     }
-
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> ans;
         vector<int> temp;
-        function(nums,ans,temp,0);
+        func(0,temp,ans,nums);
         return ans;
     }
 };
