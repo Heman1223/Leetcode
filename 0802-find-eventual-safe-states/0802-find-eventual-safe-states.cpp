11c49d806e18d0d937d6c7daf485941495375737
Heman Sharma <152138748+Heman1223@users.noreply.github.com>
2026-05-03 00:31:05 +0530

    Time: 58 ms (18.94%), Space: 66.2 MB (47.54%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -3,30 +3,29 @@ public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int n = graph.size();
         int m = graph[0].size();
-
-        vector<int> outdegree(n,0);
-        vector<vector<int>> revadj(n);
-        for(int i = 0 ;i < n;i++){
-            for(int it : graph[i]){
-                revadj[it].push_back(i);
-                outdegree[i]++;
+        vector<vector<int>> adj(n);
+        vector<int> outdoor(n,0);
+        for(int i = 0 ; i < n ; i++){
+            for(auto &it : graph[i]){
+                adj[it].push_back(i);
+                outdoor[i]++;
             }
         }
-        vector<int> ans;
         queue<int> q;
-        for(int i = 0;i < n;i++){
-            if(outdegree[i] == 0){
+        vector<int> ans;
+
+        for(int i = 0 ; i < n ; i++){
+            if(outdoor[i] == 0){
                 q.push(i);
-                
             }
         }
         while(!q.empty()){
             int node = q.front();
             ans.push_back(node);
             q.pop();
-            for(auto it : revadj[node]){
-                outdegree[it]--;
-                if(outdegree[it] == 0){
+            for(int it : adj[node]){
+                outdoor[it]--;
+                if(outdoor[it] == 0){
                     q.push(it);
                 }
             }
