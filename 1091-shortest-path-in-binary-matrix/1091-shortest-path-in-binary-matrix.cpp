5212c2e0970e5f96b79a29387ce9f1542042c310
Heman Sharma <152138748+Heman1223@users.noreply.github.com>
2026-05-10 00:17:26 +0530

    Time: 10 ms (87.86%), Space: 23.4 MB (87.93%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,33 +1,32 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        int row = grid.size();
-        int col = grid[0].size();
-        if(grid[0][0] == 1 || grid[row - 1][col - 1] == 1){
+        int n = grid.size();
+        if(grid[0][0] == 1 || grid[n - 1][n - 1] == 1){
             return -1;
         }
-        int count = 1;
         queue<pair<int,int>> q;
         q.push({0,0});
+        int count = 1;
         grid[0][0] = 1;
-        vector<int> dx = {1,-1,0,0,1,1,-1,-1};
-        vector<int> dy = {0,0,1,-1,1,-1,1,-1};
-        
+
+        int dx[] = {0,0,1,-1,1,1,-1,-1};
+        int dy[] = {1,-1,0,0,1,-1,1,-1};
         while(!q.empty()){
             int size = q.size();
-            for(int i = 0;i < size;i++){
+            for(int i = 0; i < size;i++){
                 int x = q.front().first;
                 int y = q.front().second;
                 q.pop();
-                if(x == row - 1 && y == col - 1){
+                if(x == n - 1 && y == n - 1){
                     return count;
                 }
-                for(int j = 0; j < 8;j++){
-                    int currx = x + dx[j];
-                    int curry = y + dy[j];
-                    if(currx >= 0 && currx < row && curry >= 0 && curry < col && grid[currx][curry] == 0){
-                        grid[currx][curry] = 1;
-                        q.push({currx,curry});
+                for(int j = 0;j < 8;j++){
+                    int nx = x + dx[j];
+                    int ny = y + dy[j];
+                    if(nx >= 0 && nx < n && ny >= 0 && ny < n && grid[nx][ny] == 0){
+                        q.push({nx,ny});
+                        grid[nx][ny] = 1;
                     }
                 }
             }
