596910f7af4f3b16a23d82ea01095806a8453c22
Heman Sharma <152138748+Heman1223@users.noreply.github.com>
2026-05-13 00:38:39 +0530

    Time: 39 ms (60.68%), Space: 97.3 MB (85.92%) - LeetHub

--- 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.cpp
+++ 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
 
     static bool cmp(vector<int>& a, vector<int>& b) {
-        return (a[1] - a[0]) > (b[1] - b[0]);
+        return (a[1] - a[0]) < (b[1] - b[0]);
     }
 
     int minimumEffort(vector<vector<int>>& tasks) {
@@ -10,19 +10,12 @@ public:
         sort(tasks.begin(), tasks.end(), cmp);
 
         int ans = 0;
-        int energy = 0;
 
-        for (auto &t : tasks) {
+        for (auto &task : tasks) {
+            int actual = task[0];
+            int minimum = task[1];
 
-            int actual = t[0];
-            int minimum = t[1];
-
-            if (energy < minimum) {
-                ans += (minimum - energy);
-                energy = minimum;
-            }
-
-            energy -= actual;
+            ans = max(ans + actual, minimum);
         }
 
         return ans;
