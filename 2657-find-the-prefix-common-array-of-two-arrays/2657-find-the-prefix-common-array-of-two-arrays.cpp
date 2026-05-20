51861b5718a63aeefaff28b9a9ebcdf98d873fe2
Heman Sharma <152138748+Heman1223@users.noreply.github.com>
2026-05-20 15:20:41 +0530

    Time: 4 ms (64.74%), Space: 85.8 MB (76.5%) - LeetHub

--- 2657-find-the-prefix-common-array-of-two-arrays/2657-find-the-prefix-common-array-of-two-arrays.cpp
+++ 2657-find-the-prefix-common-array-of-two-arrays/2657-find-the-prefix-common-array-of-two-arrays.cpp
@@ -1,27 +1,54 @@
 class Solution {
 public:
     vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
-        int common = 0;
         int n = A.size();
-        set<int> setA;
-        set<int> setB;
-        vector<int> c(n);
-        for(int i = 0;i < A.size();i++){
-            setA.insert(A[i]);
-            setB.insert(B[i]);
-            if(A[i] == B[i]){
+
+        vector<int> ans(n);
+        vector<int> freq(n + 1, 0);
+
+        int common = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            freq[A[i]]++;
+            if (freq[A[i]] == 2)
                 common++;
-                c[i] = common;
-                continue;
-            }
-            if(setA.count(B[i])){
+
+            freq[B[i]]++;
+            if (freq[B[i]] == 2)
                 common++;
-            }
-            if(setB.count(A[i])){
-                common++;
-            }
-            c[i] = common;
+
+            ans[i] = common;
         }
-        return c;
+
+        return ans;
     }
 };
+
+// class Solution {
+// public:
+//     vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
+//         int common = 0;
+//         int n = A.size();
+//         set<int> setA;
+//         set<int> setB;
+//         vector<int> c(n);
+//         for(int i = 0;i < A.size();i++){
+//             setA.insert(A[i]);
+//             setB.insert(B[i]);
+//             if(A[i] == B[i]){
+//                 common++;
+//                 c[i] = common;
+//                 continue;
+//             }
+//             if(setA.count(B[i])){
+//                 common++;
+//             }
+//             if(setB.count(A[i])){
+//                 common++;
+//             }
+//             c[i] = common;
+//         }
+//         return c;
+//     }
+// };
