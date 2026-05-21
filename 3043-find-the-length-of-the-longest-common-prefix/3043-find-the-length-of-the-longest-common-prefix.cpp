1527accd10d780ff7659ff5ee12d2793db830125
Heman Sharma <152138748+Heman1223@users.noreply.github.com>
2026-05-21 14:48:07 +0530

    Time: 363 ms (19%), Space: 152.7 MB (63.33%) - LeetHub

--- 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.cpp
+++ 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.cpp
@@ -2,25 +2,31 @@ class Solution {
 public:
     int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
         int ans = 0;
-        unordered_map<string,int> mp;
-        for(int i = 0;i < arr1.size();i++){
-            string s = to_string(arr1[i]);
-            string str = "";
-            for(int j = 0;j < s.size();j++){
-                str += s[j];
-                mp[str] = str.size();
+        unordered_set<string> st;
+
+        for (int x : arr1) {
+            string s = to_string(x);
+            string pref = "";
+
+            for (char c : s) {
+                pref += c;
+                st.insert(pref);
             }
         }
-        for(int i = 0;i < arr2.size();i++){
-            string str = to_string(arr2[i]);
-            string s = "";
-            for(int j = 0; j < str.size();j++){
-                s += str[j];
-                if(mp.count(s)){
-                    ans = max(ans,mp[s]);
+
+        for (int x : arr2) {
+            string s = to_string(x);
+            string pref = "";
+
+            for (char c : s) {
+                pref += c;
+
+                if (st.count(pref)) {
+                    ans = max(ans, (int)pref.size());
                 }
             }
         }
+
         return ans;
     }
 };
