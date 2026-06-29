8c1dac975d238ea476ff2eab4f6a6dc995b37389
Heman Sharma <152138748+Heman1223@users.noreply.github.com>
2026-06-29 14:48:13 +0530

    Time: 0 ms (100%), Space: 10.5 MB (67.34%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
-    void func(int i,vector<int> &temp,vector<vector<int>> &ans,vector<int> &nums){
-        if(i == nums.size()){
+    void func(int idx,vector<int>& nums,vector<int>& temp,vector<vector<int>>& ans){
+        if(idx == nums.size()){
             ans.push_back(temp);
             return;
         }
-        temp.push_back(nums[i]);
-        func(i + 1,temp,ans,nums);
+        temp.push_back(nums[idx]);
+        func(idx + 1,nums,temp,ans);
         temp.pop_back();
-        while(i + 1 < nums.size() && nums[i] == nums[i + 1]) i++;
-        func(i + 1,temp,ans,nums);
-        return;
+        while(idx + 1 < nums.size() && nums[idx] == nums[idx + 1]) idx++;
+        func(idx + 1,nums,temp,ans);
+        
     }
-
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        vector<int> temp;
         vector<vector<int>> ans;
-        func(0,temp,ans,nums);
+        vector<int> temp;
+        sort(nums.begin(),nums.end());
+        func(0,nums,temp,ans);
         return ans;
     }
 };
