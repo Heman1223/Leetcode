a43c108d15f9e7336bab5a26032eaddb8d88bde3
Heman Sharma <152138748+Heman1223@users.noreply.github.com>
2026-07-07 16:24:57 +0530

    Time: 0 ms (100%), Space: 13.8 MB (91.1%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,50 +1,44 @@
 class Solution {
 public:
-    // DFS to mark safe cells connected to the border
-    void dfs(vector<vector<char>>& board, int i, int j, int row, int col) {
-        if(i < 0 || j < 0 || i >= row || j >= col || board[i][j] != 'O') {
+    void dfs(int r,int c,vector<vector<char>> &board,int n,int m){
+        if(r < 0 || c < 0 || r >= n || c >= m || board[r][c] != 'O'){
             return;
         }
-        board[i][j] = 'S'; // mark as safe
-        dfs(board, i + 1, j, row, col);
-        dfs(board, i, j + 1, row, col);
-        dfs(board, i - 1, j, row, col);
-        dfs(board, i, j - 1, row, col);
+        board[r][c] = 'S';
+        dfs(r + 1, c ,board, n ,m);
+        dfs(r - 1, c ,board, n ,m);
+        dfs(r , c + 1 ,board, n ,m);
+        dfs(r , c - 1 ,board, n ,m);
         return;
     }
-
     void solve(vector<vector<char>>& board) {
-        int row = board.size();
-        int col = board[0].size();
-        
-        // Run DFS on all border 'O' cells to mark safe regions
-        for(int i = 0; i < row; i++) {
-            if(board[i][0] == 'O') {
-                dfs(board, i, 0, row, col);
-            }
-            if(board[i][col - 1] == 'O') {
-                dfs(board, i, col - 1, row, col);
-            }
-        }
-        for(int j = 0; j < col; j++) {
-            if(board[0][j] == 'O') {
-                dfs(board, 0, j, row, col);
-            }
-            if(board[row - 1][j] == 'O') {
-                dfs(board, row - 1, j, row, col);
-            }
-        }
+        int n = board.size();
+        int m = board[0].size();
 
-        // Flip the surrounded 'O' to 'X', and safe 'S' back to 'O'
-        for(int i = 0; i < row; i++) {
-            for(int j = 0; j < col; j++) {
-                if(board[i][j] == 'O') {
-                    board[i][j] = 'X';
-                } else if(board[i][j] == 'S') {
+        for(int i = 0;i < n;i++){
+            if(board[i][0] == 'O'){
+                dfs(i,0,board,n,m);
+            }
+            if(board[i][m - 1] == 'O'){
+                dfs(i,m - 1,board,n,m);
+            }
+        }
+        for(int i = 0;i < m;i++){
+            if(board[0][i] == 'O'){
+                dfs(0,i,board,n,m);
+            }
+            if(board[n - 1][i] == 'O'){
+                dfs(n - 1,i,board,n,m);
+            }
+        }
+        for(int i = 0;i < n;i++){
+            for(int j = 0;j < m;j++){
+                if(board[i][j] == 'S'){
                     board[i][j] = 'O';
+                }else{
+                    board[i][j] = 'X';
                 }
             }
         }
     }
-
 };
