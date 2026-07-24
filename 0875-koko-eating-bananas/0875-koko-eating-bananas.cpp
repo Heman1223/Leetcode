f77b6805543c604817ea57d41effd21adc8bee08
Heman Sharma <152138748+Heman1223@users.noreply.github.com>
2026-07-24 11:52:33 +0530

    Time: 5 ms (79.7%), Space: 23 MB (17.05%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,34 +1,30 @@
 class Solution {
 public:
-    bool possible(vector<int>& piles,int h,int speed){
-        long long hour = 0;
+    bool possible(vector<int>& piles,int mid,int h){
+        long long hours = 0;
         for(int pile : piles){
-            if(speed >= pile){
-                hour++;
+            if(pile < mid){
+                hours++;
             }else{
-                hour += (pile + speed - 1) / speed;
+                hours += (pile + mid - 1)/mid;
             }
         }
-        if(hour <= h) return true;
-        return false;
+        return hours <= h;
     }
     int minEatingSpeed(vector<int>& piles, int h) {
-        int n = piles.size();
-        int end = 0;
-        for(int pile : piles){
-            end = max(pile,end);
-        }
-        int start = 1;
-        int speed = end;
-        while(start <= end){
-            int mid = (start + end)/2;
-            if(possible(piles,h,mid)){
-                speed = min(mid,speed);
-                end = mid - 1;
+        int s = 1;
+        int e = *max_element(piles.begin(),piles.end());
+        int ans = INT_MAX;
+        
+        while(s <= e){
+            int mid = (s + e)/2;
+            if(possible(piles,mid,h)){
+                ans = min(mid,ans);
+                e = mid - 1;
             }else{
-                start = mid + 1;
+                s = mid + 1;
             }
         }
-        return speed;
+        return ans;
     }
 };
