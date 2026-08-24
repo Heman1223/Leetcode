aa05bfa59000ffb8e09d185c8864a8c18095c506
Heman Sharma <152138748+Heman1223@users.noreply.github.com>
2026-08-24 10:10:17 +0530

    Time: 0 ms (100%), Space: 16.6 MB (86.38%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -4,11 +4,11 @@ public:
         int n = grid.size();
         int m = grid[0].size();
         int fresh = 0;
-        queue<pair<int,int>> q;
         int min = 0;
+        queue<pair<int,int>> q;
 
-        for(int i = 0;i < n ; i++){
-            for(int j = 0; j < m;j++){
+        for(int i = 0;i < n;i++){
+            for(int j = 0;j < m;j++){
                 if(grid[i][j] == 2){
                     q.push({i,j});
                 }else if(grid[i][j] == 1){
@@ -26,19 +26,17 @@ public:
                 int y = q.front().second;
                 q.pop();
                 for(int j = 0;j < 4;j++){
-                    int nx = x + dx[j];
-                    int ny = y + dy[j];
-                    
-                    if(nx >= 0 && nx < n && ny >= 0 && ny < m && grid[nx][ny] == 1){
+                    int nx = dx[j] + x;
+                    int ny = dy[j] + y;
+                    if(nx >=0 && nx < n && ny >=0 && ny < m && grid[nx][ny] == 1){
+                        grid[nx][ny] = 2;
                         fresh--;
                         q.push({nx,ny});
-                        grid[nx][ny] = 2;
                     }
                 }
             }
             if(!q.empty()) min++;
         }
-        if(fresh == 0) return min;
-        return -1;
+        return fresh!=0?-1:min;
     }
 };
