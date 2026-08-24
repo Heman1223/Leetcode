8047b77712231c0ad41175b9da8cd038d11cad08
Heman Sharma <152138748+Heman1223@users.noreply.github.com>
2026-08-24 17:20:27 +0530

    Time: 0 ms (100%), Space: 12 MB (6.52%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,35 +1,24 @@
 class Solution {
 public:
+    int solve(int i,int j, vector<vector<int>> &obstacleGrid,vector<vector<int>> &dp,int m,int n){
+        if(i >= m || j >= n){
+            return 0;
+        }
+        if(obstacleGrid[i][j] == 1){
+            return 0;
+        }
+        if(i == m - 1 && j == n - 1){
+            return 1;
+        }
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        return dp[i][j] = (solve(i,j + 1,obstacleGrid,dp,m,n) + solve(i + 1,j,obstacleGrid,dp,m,n));
+    }
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        int row = obstacleGrid.size();
-        int col = obstacleGrid[0].size();
-        if(obstacleGrid[0][0] == 1) return 0;
-        
-        obstacleGrid[0][0] = 1;
-        
-        for(int j = 1;j < col;j++){
-            if(obstacleGrid[0][j] == 0 && obstacleGrid[0][j - 1] == 1){
-                obstacleGrid[0][j] = 1;
-            }else{
-                obstacleGrid[0][j] = 0;
-            }
-        }
-        for(int i = 1;i < row;i++){
-            if(obstacleGrid[i][0] == 0 && obstacleGrid[i - 1][0] == 1){
-                obstacleGrid[i][0] = 1;
-            }else{
-                obstacleGrid[i][0] = 0;
-            }
-        }
-        for(int i = 1;i<row;i++){
-            for(int j = 1;j<col;j++){
-                if(obstacleGrid[i][j] == 1){
-                    obstacleGrid[i][j] = 0;
-                }else{
-                    obstacleGrid[i][j] = obstacleGrid[i - 1][j] + obstacleGrid[i][j - 1];
-                }
-            }
-        }
-        return obstacleGrid[row - 1][col - 1];
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+        vector<vector<int>> dp(m,vector<int> (n,-1));
+        return solve(0,0,obstacleGrid,dp,m,n);
     }
 };
